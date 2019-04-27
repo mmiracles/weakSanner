3b35ce20b3e44f58b05395df0bd5c66e014bdad5
xuyang <810926968@qq.com>
2019-04-27 09:34:43 +0800

    xy

--- LScan.py
+++ LScan.py
@@ -7,109 +7,135 @@ from mongo import MongoScanner
 from pyredis import RedisScanner
 from sqlserver import SqlServerScanner
 from tomcat import TomcatScanner
-
+import argparse
 import threading
 
-ip = '129.204.234.135'
+RESULT_FILE = './result.txt'
+PWD_FILE = './pwdTest.txt'
 
-def ftpConnect(ip, pwdLines):
+
+def ftpConnect(host, pwdLines):
     try:
-        with open('./result.txt', 'a') as resultFile:
-            ftpSanner = FTPScanner(ip, 22)
+        with open(RESULT_FILE, 'a') as resultFile:
+            ftpSanner = FTPScanner(host, 22)
             ftpRes = ftpSanner.scanWeakPwsd(pwdLines)
             resultFile.write(ftpRes)
     except Exception as e:
         print('ftpConnectError:', e)
 
 
-def telnetConnect(ip, pwdLines):
+def telnetConnect(host, pwdLines):
     try:
-        with open('./result.txt', 'a') as resultFile:
-            telnetScanner = TelnetScanner(ip, 23)
+        with open(RESULT_FILE, 'a') as resultFile:
+            telnetScanner = TelnetScanner(host, 23)
             telnetRes = telnetScanner.scanWeakPwsd(pwdLines)
             resultFile.write(telnetRes)
     except Exception as e:
         print('telnetConnectError:', e)
 
 
-def sshConnect(ip, pwdLines):
+def sshConnect(host, pwdLines):
     try:
-        with open('./result.txt', 'a') as resultFile:
-            sshScanner = SSHScanner(ip)
+        with open(RESULT_FILE, 'a') as resultFile:
+            sshScanner = SSHScanner(host)
             sshRes = sshScanner.scanWeakPwsd(pwdLines)
             resultFile.write(sshRes)
     except Exception as e:
         print('sshConnectError:', e)
 
 
-def mysqlConnect(ip, pwdLines):
+def mysqlConnect(host, pwdLines):
     try:
-        with open('./result.txt', 'a') as resultFile:
-            mysqlScanner = MysqlScanner(ip)
+        with open(RESULT_FILE, 'a') as resultFile:
+            mysqlScanner = MysqlScanner(host)
             mysqlRes = mysqlScanner.scanWeakPwsd(pwdLines)
             resultFile.write(mysqlRes)
     except Exception as e:
         print('mysqlConnectError:', e)
 
 
-def postgresConnect(ip, pwdLines):
+def postgresConnect(host, pwdLines):
     try:
-        with open('./result.txt', 'a') as resultFile:
-            pgScanner = PostgresScanner(ip)
+        with open(RESULT_FILE, 'a') as resultFile:
+            pgScanner = PostgresScanner(host)
             pgRes = pgScanner.scanWeakPwsd(pwdLines)
             resultFile.write(pgRes)
     except Exception as e:
         print('pgsqlConnectError:', e)
 
 
-def mongoConnect(ip, pwdLines):
+def mongoConnect(host, pwdLines):
     try:
-        with open('./result.txt', 'a') as resultFile:
-            mongoScanner = MongoScanner(ip)
+        with open(RESULT_FILE, 'a') as resultFile:
+            mongoScanner = MongoScanner(host)
             mongoRes = mongoScanner.scanWeakPwsd(pwdLines)
             resultFile.write(mongoRes)
     except Exception as e:
         print('mongoConnectError:', e)
 
-def redisConnect(ip, pwdLines):
+
+def redisConnect(host, pwdLines):
     try:
-        with open('./result.txt', 'a') as resultFile:
-            redisScanner = RedisScanner(ip)
+        with open(RESULT_FILE, 'a') as resultFile:
+            redisScanner = RedisScanner(host)
             redisRes = redisScanner.scanWeakPwsd(pwdLines)
             resultFile.write(redisRes)
     except Exception as e:
         print('redisConnectError:', e)
 
-def sqlServerConnect(ip, pwdLines):
+
+def sqlServerConnect(host, pwdLines):
     try:
-        with open('./result.txt', 'a') as resultFile:
-            sqlScanner = SqlServerScanner(ip)
+        with open(RESULT_FILE, 'a') as resultFile:
+            sqlScanner = SqlServerScanner(host)
             sqlRes = sqlScanner.scanWeakPwsd(pwdLines)
             resultFile.write(sqlRes)
     except Exception as e:
         print('sqlServerConnectError:', e)
-    
-def tomcatConnect(ip, pwdLines):
+
+
+def tomcatConnect(host, pwdLines):
     try:
-        with open('./result.txt', 'a') as resultFile:
-            tomcatScanner = TomcatScanner(ip)
+        with open(RESULT_FILE, 'a') as resultFile:
+            tomcatScanner = TomcatScanner(host)
             tomcatRes = tomcatScanner.scanWeakPwsd(pwdLines)
             resultFile.write(tomcatRes)
     except Exception as e:
         print('tomcatConnectError:', e)
 
-result = []
-try:
-    with open('./pwdTest.txt') as pwdFile:
-        pwdLines = pwdFile.readlines()
-        # threading.Thread(target=ftpConnect, args=(ip, pwdLines)).start()
-        # threading.Thread(target=telnetConnect, args=(ip, pwdLines)).start()
-        # threading.Thread(target=sshConnect, args=(ip, pwdLines)).start()
-        # threading.Thread(target=mysqlConnect, args=(ip, pwdLines)).start()
-        # threading.Thread(target=postgresConnect, args=(ip, pwdLines)).start()
-        # threading.Thread(target=mongoConnect, args=(ip, pwdLines)).start()
-        threading.Thread(target=redisConnect, args=(ip, pwdLines)).start()
-        # threading.Thread(target=tomcatConnect, args=(ip, pwdLines)).start()
-        # threading.Thread(target=sqlServerConnect, args=(ip, pwdLines)).start()
-except Exception as e:
-    print(e)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='LXM Scanner')
+    parser.add_argument('-H', dest='hostname', help='The host name')
+    parser.add_argument('-f', dest='pwdFile', help='Password dictionary file')
+    parser.add_argument('-r', dest='resultFile', help='Result dictionary file')
+    params = None
+    try:
+        params = parser.parse_args()
+    except:
+        print(parser.parse_args(['-h']))
+        exit(0)
+
+    host = str(params.hostname)
+    if params.pwdFile:
+        pwdFile = params.pwdFile
+    if host == 'None':
+        print(parser.parse_args(['-h']))
+        exit(0)
+
+    result = []
+    try:
+        with open(PWD_FILE) as pwdFile:
+            pwdLines = pwdFile.readlines()
+            # threading.Thread(target=ftpConnect, args=(host, pwdLines)).start()
+            # threading.Thread(target=telnetConnect, args=(host, pwdLines)).start()
+            # threading.Thread(target=sshConnect, args=(host, pwdLines)).start()
+            threading.Thread(target=mysqlConnect, args=(host, pwdLines)).start()
+            # threading.Thread(target=postgresConnect, args=(host, pwdLines)).start()
+            # threading.Thread(target=mongoConnect, args=(host, pwdLines)).start()
+            threading.Thread(target=redisConnect,
+                             args=(host, pwdLines)).start()
+            # threading.Thread(target=tomcatConnect, args=(host, pwdLines)).start()
+            # threading.Thread(target=sqlServerConnect, args=(host, pwdLines)).start()
+    except Exception as e:
+        print(e)
